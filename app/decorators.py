7e1c65b39d1ae13601b801135122574945b82182
El Mehdi Rami <lbiggboss19@gmail.com>
2021-03-10 03:18:14 +0100

    added is_json decorator

---
+++ app/decorators.py
@@ -0,0 +1,11 @@
+from flask import request
+from functools import wraps
+
+
+def is_json(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not request.is_json:
+            return {'success': False}, 400
+        return f(*args, **kwargs)
+    return wrapper
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from flask import request
 from app import app
+from app.decorators import is_json
 
 
 @app.route('/')
 def home():
     return {'message': 'Hello Flask!'}
+
+
+@app.route('/service', methods=['POST'])
+@is_json
+def service():
+    return {'success': True}
